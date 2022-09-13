7cf15ef70c4cf5f54db02492b3befb183e7ae4b3
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-09-13 11:42:34 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function prints the last digit of a number
+ *@n: input variable - number being tested
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	n %= 10;
+
+	if (n < 0)
+		n = -n;
+		_putchar(n + '0');
+		return (n);
+}
