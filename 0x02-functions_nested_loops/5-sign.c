d92a9b7b4cad0761ca8031e28bea75c6892b200e
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-09-13 11:24:14 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_sign - function to print sign of number.
+ * @n: funct takes int argument n.
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
