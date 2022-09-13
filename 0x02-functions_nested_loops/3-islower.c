44838f0c0db9a22fab9f4d96da3eef4071127195
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-09-13 10:37:18 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks lowercase characters
- * @c is the int value argument of the function
+ * @c: is the int value argument of the function
  * Return: always 0
  */
 int _islower(int c)
