1cfa396e0e8eb43bb854f2191a42c021717d41e2
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-09-13 10:09:08 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c, j;
+
+	j = 0;
+	while ( j <= 9)
+	{
+		c = 'a'
+		while ( c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		J++;
+
+	}
+
+}
