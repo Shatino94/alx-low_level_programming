9dee8fac6e56a4780a5e41cffcf0af43d45cd8e0
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-09-15 10:18:37 -0700

    update for loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - print out number from 0 to 9
- * Return: void
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i < 58; i++)
 	{
-		_putchar(i);
+	_putchar(i);
 	}
-	_putchar(10);
+	_putchar('\n');
 }
