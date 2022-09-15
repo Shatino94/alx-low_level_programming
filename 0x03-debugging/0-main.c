6682ef4675a932ba9f2b68293d5287528322d7d4
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-09-15 06:20:20 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function that test and print if a number is negetive or positive
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
