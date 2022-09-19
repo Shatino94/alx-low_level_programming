3fa25ab526b43839dca64bb016b1805ac95b589b
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-09-19 07:32:57 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98(int *n) - function takes int *n and update value to 98
+ *@n: pointer  to an int argument
+ *Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
