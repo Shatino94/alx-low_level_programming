f5e84c910855a938606c85d88535817d72348389
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-09-21 13:35:46 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,16 @@
  */
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int x;
+	
+	x = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		x = -x;
+		x = -n;
 	}
-	if ((x / 10) > 0)
+	if ((x / 10) != 0)
 		print_number(x / 10);
 
 	_putchar(x % 10 + '0');
