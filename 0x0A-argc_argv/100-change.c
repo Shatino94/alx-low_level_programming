3d69a3cbf034f7e3dba7d22ab4f1154a908f3007
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-09-30 15:13:44 -0700

    task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print leftover coin
+ * @argc: count
+ * @argv: string
+ * Return: return 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, n, s, coin = 0;
+	int c[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	s = atoi(argv[1]);
+	if (s < 1)
+		printf("0\n");
+	else
+	{
+		for (i = 0; i < 5 && s; i++)
+		{
+			n = s / c[i];
+			ch += n;
+			s -= n * c[i];
+		}
+		printf("%d\n", coin);
+	}
+	return (0);
+}
