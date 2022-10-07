e3f862290c925d79000c964865c70b23eea48e93
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-10-06 17:45:18 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
 /**
  * main - prototypes header file
  * void prototypes (void)
@@ -12,8 +14,15 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)i;
 int _putchar(char);
+int len(char *str);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 
 
 #endif /* MAIN_H */
