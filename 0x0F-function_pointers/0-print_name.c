86f2df73975095bfdddde362523331ad7300e66f
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-10-12 09:13:55 -0700

    updates

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  *print_name - function's name
@@ -10,7 +11,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
 	f(name);
 }
