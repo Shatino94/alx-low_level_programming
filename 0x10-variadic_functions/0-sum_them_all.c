473273bc54940e28518d81499870dd2366875789
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-10-13 20:10:32 -0700

    final update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,19 +10,19 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list list;
+	va_list valist;
 	int sum = 0;
-	unsigned int i;
+	unsigned int index;
 
 	if (n == 0)
 		return (0);
 
-	va_start(li, n)
+	va_start(valist, n)
 	
-	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
+	for (index = 0; index < n; index++)
+		sum += va_arg(valist, int);
 
-	va_end(list);
+	va_end(valist);
 
 	return (sum);
 }
