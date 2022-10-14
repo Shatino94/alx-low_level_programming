38e6e3ed20044541e1b01e86bfe959d0baac66c5
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-10-13 19:56:19 -0700

    updates

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,15 +16,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(li, n);
 	
-	if (n == 0)
-		return(0);
-
-	if (n != 0)
-	{
-		for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(li, int);
-	}
 
 	va_end(li);
+
 	return (sum);
 }
